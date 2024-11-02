dc911c2ce0ac075b6d6cdc4e02d2f33eae8e7a2f
Alfredo Filigheddu <filigheddualfredo@gmail.com>
2024-11-02 19:09:47 +0100

    feat: added loader to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,27 @@ import { Component } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
 import { RouterOutlet } from "@angular/router";
 import { TranslateModule, TranslateService } from "@ngx-translate/core";
+import { MatLoaderComponent } from "@shared";
+import { AppStatusService } from "@core";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet, TranslateModule, MatIconModule],
+  imports: [RouterOutlet, TranslateModule, MatIconModule, MatLoaderComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = "hospital-demo";
+  isSpinnerVisible: boolean = false;
 
-  constructor(public translateService: TranslateService) {
+  constructor(private translateService: TranslateService, private appStatusService: AppStatusService) {
     translateService.setDefaultLang("en");
     translateService.use("en");
+    this.subscriberToSpinnerStatus();
+  }
+
+  subscriberToSpinnerStatus() {
+    this.appStatusService.isSpinnerVisible.subscribe((value) => (this.isSpinnerVisible = value));
   }
 
   changeLanguage(language: string) {
